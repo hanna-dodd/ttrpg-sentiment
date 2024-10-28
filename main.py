8e4555825d1defb98957f09a2c5a0537ee389c15
Hanna Dodd <hrdodd@gmail.com>
2024-10-28 12:45:10 -0400

    2024-10-28

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 import pandas as pd
+import re
+from bs4 import BeautifulSoup
 
-file = open('naddpod/c3-e1.txt', mode = 'r', encoding = 'utf-8-sig')
-lines = file.readlines()
-file.close()
-
-# for line in lines:
+def parse_critrole():
     
-    # remove empty lines
-    # split by the colon
-    # create data frame with speaker name and line (potentially any given speech tags for the line?)
+    file = open('critrole/cr1-1.html', mode = 'r', encoding = 'utf-8-sig')
+    lines = file.readlines()
+    file.close()
+    
+    df = pd.DataFrame({"name": [], "line": []})
+    
+    for line in lines:
+        
+        line = line.strip()
+        
+        if line.startswith("<dt>"):
+            name = find_name(line)
+        elif line.startswith("<dd"):
+            # add name as first column
+            speech = find_speech(line)
+            row = {"name": name, "line": speech}
+            df = df._append(row, ignore_index=True)
+    
+    df.to_csv("cr1-1.csv", index=False)
+
+
+def find_name(input_text):
+    text = re.findall(r"[\w']+", input_text)
+    return text[11]
+
+def find_speech(input_text):
+    soup = BeautifulSoup(input_text, 'html.parser')
+    return soup.get_text()
+
+parse_critrole()
